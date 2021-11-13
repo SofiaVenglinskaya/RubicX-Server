5a476f0d184a0280b3308946e47ea3131a70bd73
yourboyfriendsdrug <74487159+yourboyfriendsdrug@users.noreply.github.com>
2021-11-13 17:54:47 +0300

    Сервер

---
+++ RubicX-Server.DataAccess.Core/Interfaces/Contexts/IRubicContext.cs
@@ -0,0 +1,15 @@
+﻿using RubicX_Server.DataAccess.Core.Models;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RubicX_Server.DataAccess.Core.Interfaces.Contexts
+{
+    public interface IRubicContext : IDisposable, IAsyncDisposable
+    {
+        DbSet<UserRto> Users { get; set; }
+        DbSet<UserRoleRto> UserRoles { get; set; }
+        Task<int> SaveCahangesAsynk(CancellationToken cancellationToken = default);
+    }
+
+  
---
+++ RubicX-Server.DataAccess.Core/Models/UserRto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RubicX_Server.DataAccess.Core.Models
+{
+    [Table("User")]
+    public class UserRto
+    {
+        [Key] public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string AvatarUrl { get; set; }
+        public string Introduction { get; set; }
+        public UserRoleRto Role { get; set; }
+        [NotMapped]
+        public string GetFullname 
+        {
+            get => LastName + " " + Firstname + " " + Patronymic;
+
+        }
+    }
+}
